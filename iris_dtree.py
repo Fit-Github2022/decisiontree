17e88cf2c3112f32d35d822b20048776a3906d76
Fit-Github2022 <115910564+Fit-Github2022@users.noreply.github.com>
2022-10-23 11:01:42 +0800

    Update iris_dtree.py

--- iris_dtree.py
+++ iris_dtree.py
@@ -1,12 +1,11 @@
 import pandas as pd
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
 
-iris = load_iris()
 (X_iris, y_iris) = load_iris(return_X_y = True)
 X_train, X_test, y_train, y_test = train_test_split(X_iris, y_iris, random_state = 0)
 
-from sklearn.datasets import load_iris
 from sklearn import tree
 from sklearn.tree import plot_tree
 
@@ -17,4 +16,5 @@ clf = clf.fit(X_train, y_train)
 clf.fit(X_train, y_train)
 
 tree.plot_tree(clf.fit(X_train, y_train) )
+
 clf.score(X_test, y_test)
